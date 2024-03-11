9b8c808cd2391af3fa1d7cb1bb3727ef19f18ca7
Jose Jacquez <23jose.serna@gmail.com>
2024-03-11 15:17:00 +0000

    Building data set

--- DMML_Project.R
+++ DMML_Project.R
@@ -1,1 +1,63 @@
 #DMML Group Project
+set.seed(555)
+####################################
+# Building the data set
+####################################
+library(fastDummies)
+
+#read data
+drug <- read.csv("group_22.csv") 
+
+#eliminate over-claimers
+drug <- drug %>% filter(Semer=="CL0")
+
+
+#since these variables do not have an inherent hierarchy 
+#convert Ethnicity and Country columns to dummies
+drug$Country <- ifelse(drug$Country==-0.09765,"Australia",
+                       ifelse(drug$Country==0.24923,"Canada",
+                              ifelse(drug$Country==-0.46841,"New_Zealand",
+                                     ifelse(drug$Country==-0.28519,"Other",
+                                            ifelse(drug$Country==0.21128,"ROI",
+                                                   ifelse(drug$Country==0.96082,"UK",
+                                                          ifelse(drug$Country==-0.57009,"USA","Unknown")))))))
+levels(factor(drug$Country))
+
+drug$Ethnicity <- ifelse(drug$Ethnicity==-0.50212,"Asian",
+                         ifelse(drug$Ethnicity==-1.10702,"Black",
+                                ifelse(drug$Ethnicity==1.90725,"Mixed_Black_Asian",
+                                       ifelse(drug$Ethnicity==0.12600,"Mixed_White_Asian",
+                                              ifelse(drug$Ethnicity==-0.22166,"Mixed_White_Black",
+                                                     ifelse(drug$Ethnicity==0.11440,"Other",
+                                                            ifelse(drug$Ethnicity==-0.31685,"White","Unknown")))))))
+
+levels(factor(drug$Ethnicity))
+
+drug <- dummy_cols(drug,c("Country", "Ethnicity"), remove_selected_columns=TRUE)
+
+
+#target variable to factor
+
+drug$Merged_Amphet <- ifelse(drug$Amphet %in% c("CL0"), "Never Used",
+                             ifelse(drug$Amphet %in% c("CL1", "CL2"), "Used Over a Year Ago",
+                                    "Used in the Last Year"))
+
+drug$Amphet <- as.factor(drug$Amphet)
+drug$Merged_Amphet <- as.factor(drug$Merged_Amphet)
+
+
+#split into training and testing
+training_n <- floor(0.8*nrow(drug))
+training_indices <- sample(c(1:nrow(drug)), training_n)
+
+#remove ID and fake drug 
+cols <- c("ID", "Semer")
+drug_numeric <- drug %>% select(-one_of(cols)) %>% 
+  relocate(Amphet, .before=Merged_Amphet)
+
+train <- drug_numeric[training_indices, ]
+test <- drug_numeric[-training_indices, ]
+
+
+
+
