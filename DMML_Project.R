c0fae4149dfed167346f62f6d0ea6c5a70df46fb
Jose Jacquez <23jose.serna@gmail.com>
2024-03-12 00:04:24 +0000

    Neural Network Code

--- DMML_Project.R
+++ DMML_Project.R
@@ -59,5 +59,60 @@ train <- drug_numeric[training_indices, ]
 test <- drug_numeric[-training_indices, ]
 
 
+#######################
+# Neural Network
+#######################
+
+library(neuralnet)
+library(NeuralNetTools)
+
+#pca since the model  training is too slow 
+nn_train <- train ; nn_test <- test
+#my.pca <- princomp(nn_train[,-c(25,26)], cor=TRUE) #correlation-based PCA
+#summary(my.pca) #the first 18 components contain 90% of the variance
+
+#nn_train <- my.pca$scores[,1:18] %>% as.data.frame() %>% mutate(Merged_Amphet = nn_train$Merged_Amphet)
+#nn_test <- predict(my.pca, nn_test[,-c(25,26)])[,1:18] %>% as.data.frame() %>% mutate(Merged_Amphet = nn_test$Merged_Amphet)
+
+#checking correlation of covariates against target
+corr <- abs(cor(nn_train[,-c(25)]%>%mutate(Merged_Amphet=as.numeric(as.numeric(nn_train$Merged_Amphet))))[-25,25]) %>% as.data.frame()
+colnames(corr) <- c("Abs_Cor")
+corr <- cbind(var = rownames(corr), corr)
+rownames(corr) <- 1:nrow(corr)
+corr %>% arrange(desc(Abs_Cor)) %>% head(10)
+
+# min-max normalisation
+maxs <- apply(nn_train[,-c(25,26)], 2, max)
+mins <- apply(nn_train[,-c(25,26)], 2, min)
+nn_train[,-c(25,26)] <- as.data.frame(scale(nn_train[,-c(25,26)],center = mins, scale = maxs - mins))
+nn_test[,-c(25,26)] <- as.data.frame(scale(nn_test[,-c(25,26)],center = mins, scale = maxs - mins))
+
+
+#train neural network
+softplus <- function(x) log(1+exp(x))
+
+
+#nn=neuralnet(Merged_Amphet~Age+Gender+Education+Nscore+Escore+Oscore+
+#               Ascore+Cscore+Impulsive+Impulsive+SS+
+#               Country_Australia+Country_Canada+Country_New_Zealand+Country_Other+Country_ROI+Country_UK+Country_USA+
+#              Ethnicity_Asian+Ethnicity_Black+Ethnicity_Mixed_Black_Asian+Ethnicity_Mixed_White_Asian+Ethnicity_Mixed_White_Black+Ethnicity_Other+Ethnicity_White,
+#             data=nn_train, hidden=c(2,2,2,2,2,2,2,2),rep=10,err.fct = "sse",lifesign="full",act.fct="tanh",linear.output=FALSE)
+#SS+Country_UK+Impulsive+Country_USA+Oscore+Gender+Cscore+Education+Ascore+Age+Ethnicity_Black
+nn=neuralnet(Merged_Amphet~.,
+           data=nn_train[,-25], hidden=c(14),rep=2,err.fct = "ce",stepmax=150000,lifesign="full",act.fct="logistic",linear.output=FALSE)
+
+#Predict and evaluate training and testing accuracy
+#Training accuracy
+nn_train_pred <-
+  nn_train %>% mutate(pred = max.col(predict(nn, nn_train))) %>% 
+  mutate(accuracy= pred==as.numeric(nn_train$Merged_Amphet))
+mean(nn_train_pred$accuracy)
+#Testing accuracy
+nn_test_pred <-
+  nn_test %>% mutate(pred = max.col(predict(nn, nn_test))) %>% 
+  mutate(accuracy= pred==as.numeric(nn_test$Merged_Amphet))
+mean(nn_test_pred$accuracy)
+#training accuracy around 70%, testing around 57%
+
 
 
