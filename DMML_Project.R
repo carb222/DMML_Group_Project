1fcfc19e89e8fc98a18bce06492225aaabec35aa
Carlos Ballesteros <carlosballesteroscarranza@gmail.com>
2024-03-18 10:43:58 +0000

    Added format to script

--- DMML_Project.R
+++ DMML_Project.R
@@ -1,18 +1,31 @@
-#DMML Group Project
+# DMML Group Project ----
+
+#__________________________________________________________
+  
 set.seed(555)
-####################################
-# Building the data set
-####################################
+## Libraries ----
+library(ggplot2)
+library(GGally)
+library(skimr)
+library(MASS)
+library(e1071)
+library(caret)
 library(fastDummies)
+library(tidyr)
+library(dplyr)
 
-#read data
-drug <- read.csv("group_22.csv") 
+#__________________________________________________________
+  
+## Building the data set ----
 
-#eliminate over-claimers
+### Read data ----
+drug <- read.csv("group_22.csv")
+
+### Data tidying ----
+#Eliminate over-claimers
 drug <- drug %>% filter(Semer=="CL0")
 
-
-#since these variables do not have an inherent hierarchy 
+#Since these variables do not have an inherent hierarchy 
 #convert Ethnicity and Country columns to dummies
 drug$Country <- ifelse(drug$Country==-0.09765,"Australia",
                        ifelse(drug$Country==0.24923,"Canada",
@@ -37,27 +50,25 @@ drug <- dummy_cols(drug,c("Country", "Ethnicity"), remove_selected_columns=TRUE)
 
 
 #target variable to factor
-
 drug$Merged_Amphet <- ifelse(drug$Amphet %in% c("CL0"), "Never Used",
                              ifelse(drug$Amphet %in% c("CL1", "CL2"), "Used Over a Year Ago",
                                     "Used in the Last Year"))
-
-drug$Amphet <- as.factor(drug$Amphet)
-drug$Merged_Amphet <- as.factor(drug$Merged_Amphet)
-
-
-#split into training and testing
-training_n <- floor(0.8*nrow(drug))
-training_indices <- sample(c(1:nrow(drug)), training_n)
-
 #remove ID and fake drug 
 cols <- c("ID", "Semer")
 drug_numeric <- drug %>% select(-one_of(cols)) %>% 
   relocate(Amphet, .before=Merged_Amphet)
+drug$Amphet <- as.factor(drug$Amphet)
+drug$Merged_Amphet <- as.factor(drug$Merged_Amphet)
+
+#__________________________________________________________
+  
+## Split into training and testing ----
+training_n <- floor(0.8*nrow(drug))
+training_indices <- sample(c(1:nrow(drug)), training_n)
 
 train <- drug_numeric[training_indices, ]
 test <- drug_numeric[-training_indices, ]
 
-
+#__________________________________________________________
 
 
