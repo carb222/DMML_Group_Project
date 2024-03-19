ca4e106310ed6c80a448ec092b7ecd4527b27d4b
awhite5mac <117464518+awhite5mac@users.noreply.github.com>
2024-03-19 11:03:45 +0000

    Rename kNN file

--- DMML_Project.R
+++ DMML_Project.R
@@ -4,6 +4,7 @@ set.seed(555)
 # Building the data set
 ####################################
 library(fastDummies)
+library(dplyr)
 
 #read data
 drug <- read.csv("group_22.csv") 
@@ -58,6 +59,44 @@ drug_numeric <- drug %>% select(-one_of(cols)) %>%
 train <- drug_numeric[training_indices, ]
 test <- drug_numeric[-training_indices, ]
 
+
+library(class)
+
+# Leave-One-Out Cross-Validation on 7 Classes
+set.seed(555)
+K <- c(1:20)
+cv.corr <- c()
+for (k in K){
+  train.pred <- knn.cv(train[, 1:24], train[, 25], k = k)
+  cv.corr[k] <- mean(train[, 25] == train.pred)
+}
+
+plot(K, cv.corr, type = "b", ylab = "Leave-One-Out Cross-Validation CCR")
+abline(v = which.max(cv.corr), lty = 2, col = "blue")
+
+# Fitting 15-NN model
+k.opt <- which.max(cv.corr)
+test.pred <- knn(train[, 1:24], test[, 1:24], train[, 25], k = k.opt)
+# Test CCR
+mean(test[, 25] == test.pred)
+
+
+# Leave-One-Out Cross Validation on 3 Classes
+set.seed(555)
+K <- c(1:15)
+cv.corr <- c()
+for (k in K){
+  train.pred <- knn.cv(train[, 1:24], train[, 26], k = k)
+  cv.corr[k] <- mean(train[, 26] == train.pred)
+}
+
+plot(K, cv.corr, type = "b", ylab = "Leave-One-Out Cross-Validation CCR")
+abline(v = which.max(cv.corr), lty = 2, col = "blue")
+
+# Fitting 5-NN model
+k.opt <- which.max(cv.corr)
+test.pred <- knn(train[, 1:24], test[, 1:24], train[, 26], k = k.opt)
+mean(test[, 26] == test.pred)
 
 
 
